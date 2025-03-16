5b60970540a73492daa6f3a07bc35a45cd30b897
Walter Rivera Gambini <walterivgam@gmail.com>
2025-03-16 18:44:36 -0500

    Se agregaron el servidor de Eureka y el Apim

--- apim/src/main/java/edu/cibertec/AppConfiguration.java
+++ apim/src/main/java/edu/cibertec/AppConfiguration.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AppConfig {
+public class AppConfiguration {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppConfig.class, args);
+		SpringApplication.run(AppConfiguration.class, args);
 	}
 
 }
--- eurekaserver/src/main/java/edu/cibertec/AppConfiguration.java
+++ eurekaserver/src/main/java/edu/cibertec/AppConfiguration.java
@@ -2,14 +2,14 @@ package edu.cibertec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class AppConfig {
+@EnableEurekaServer
+public class AppConfiguration {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppConfig.class, args);
+		SpringApplication.run(AppConfiguration.class, args);
 	}
 
 }
